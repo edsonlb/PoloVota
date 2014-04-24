36aa1a97035e3e681bdbd2fa34366b813b8ec5dd
edsonlb <ti_edson@yahoo.com.br>
2014-04-24 10:34:29 -0300

    Elaboracao de Form Person

--- persons/views.py
+++ persons/views.py
@@ -1,14 +1,33 @@
 # This Python file uses the following encoding: utf-8
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.db.models import Q
 from persons.models import Person
+from persons.forms import AvaliadorForm
 
 def listar(request):
-    #persons = Person.objects.all()
-
-    # TESTE LOCAL PARA VERIFICAR SE A TABELA ESTA LISTANDO
-    persons = []
-    persons.append(Person(first_name='NOME1', email='MAIL1', empresa='EMPRESA1'))
-    persons.append(Person(first_name='NOME2', email='MAIL2', empresa='EMPRESA2'))
-
+    persons = Person.objects.all().order_by('first_name')
     return render(request, 'list.html', {'persons': persons})
+
+def editar(request, pk = 0):
+    try:
+        person = Person.objects.get(pk=pk)
+        form = AvaliadorForm(instance=person)
+    except:
+        return HttpResponseRedirect('/persons/')
+
+    return render(request, 'form.html', {'form': form})
+
+def salvar(request):
+    if request.method == 'POST':
+        form = AvaliadorForm(request.POST)
+        
+        if form.is_valid():
+            person = Person(**form.cleaned_data)
+            person.save()
+            return HttpResponseRedirect('/persons/')
+        else:
+            print "ERRO1"
+            return render(request, 'form.html', {'form': form})   
+    else:
+        return render(request, 'form.html', {'form': AvaliadorForm()})
+    
