411d98ade1e2b1d84b180ffd9f50ff7ed9955567
Lucas Castejon <lucascastejon@gmail.com>
2014-04-24 09:08:29 -0300

    Criando models e views do Project

---
+++ projects/forms.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+from django import forms
+from app_polo.projects.models import Projeto
+from django.forms import ModelForm, TextInput, EmailInput
+from django.utils.translation import ugettext as _
+
+class ProjetoForm(forms.ModelForm):
+	class Meta:
+		model = Projeto
+		widgets = {
+			'tema' :TextInput(attrs={'placeholder':_('Tema'), 'type':'text'}),
+			'projeto':TextInput(attrs={'placeholder':_('Link do projeto'), 'type':'text'}),
+			'nomeOrientador' :TextInput(attrs={'placeholder':_('Nome do Orientador'), 'type':'text'}),
+			'nomeIntegrante': TextInput(attrs={'placeholder':_('Nomes dos Integrantes'), 'type':'text'}),
+			'redeSocial': TextInput(attrs={'placeholder':_('Rede Social'), 'type':'text'}),
+			'modelagem': TextInput(attrs={'placeholder':_('Modelagem'), 'type':'text'}),
+			# Dados do Lider
+			'nomeLider': TextInput(attrs={'placeholder':_('Nome do Lider'), 'type':'text'}),
+			'emailLider': EmailInput(attrs={'placeholder':_('Email')}),
+			'telefoneLider': TextInput(attrs={'placeholder':_('Telefone do Lider'), 'type':'text'}),
+			'cpfLider': TextInput(attrs={'placeholder':_('CPF do Lider'), 'type':'text'})
+		}
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+def new(request):
+	return render(request, 'projects/projeto.html',
+		{'form': ProjetoForm()})
