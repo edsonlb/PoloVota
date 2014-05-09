8e57b77300f35cf88e808e84172e0dc307bed37f
edsonlb <ti_edson@yahoo.com.br>
2014-05-09 13:52:49 -0300

    Enviando projetos para master

--- polovota/urls.py
+++ polovota/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
+	#url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'core.views.index'),
     url(r'^projects/', include('projects.urls')),
     url(r'^persons/', include('persons.urls')),
