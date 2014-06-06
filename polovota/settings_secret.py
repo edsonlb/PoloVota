04bb957b6a295dc55c20841e7c9758d3fe33cb26
edsonlb <ti_edson@yahoo.com.br>
2014-06-06 19:37:02 -0300

    Ajustando Settings

--- polovota/settings_secret.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY_SETTINGS = '6oh*mmqz$1#x(r$91ry2q@c2b8je^eua9i3ie89-h!ifqg&y5!'
-SECRET_EMAIL_HOST = 'smtp.mandrillapp.com'
-SECRET_EMAIL_HOST_USER = 'contato@celuladigital.com.br'
-SECRET_EMAIL_HOST_PASSWORD = 'dDVWriiTxDC5_WbNrQWiZw'
-SECRET_DEFAULT_FROM_EMAIL = 'PoloVota <contato@celuladigital.com.br>'
