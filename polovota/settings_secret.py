99a721a1797bde73c84e10471ac4278c229c3778
edsonlb <ti_edson@yahoo.com.br>
2014-06-06 17:30:04 -0300

    SETTINGS2

--- polovota/settings_secret.py
+++ polovota/settings_secret.py
@@ -1,5 +1,4 @@
 SECRET_KEY_SETTINGS = '6oh*mmqz$1#x(r$91ry2q@c2b8je^eua9i3ie89-h!ifqg&y5!'
-
 SECRET_EMAIL_HOST = 'smtp.mandrillapp.com'
 SECRET_EMAIL_HOST_USER = 'contato@celuladigital.com.br'
 SECRET_EMAIL_HOST_PASSWORD = 'dDVWriiTxDC5_WbNrQWiZw'
